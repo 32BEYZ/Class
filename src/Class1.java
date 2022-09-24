926764e441cd64c7f3593c889c98b429a9d0efb7
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-24 14:28:14 +0300

    class1

---
+++ src/Class1.java
@@ -0,0 +1,16 @@
+
+public class Class1 {
+
+	public static void main(String[] args) {
+	CustomerManeger customerManeger	;
+	CustomerManeger customerManeger2= new CustomerManeger();
+	customerManeger=customerManeger2;
+	
+	customerManeger.Add();
+	customerManeger.Remove();
+	customerManeger.Update();
+
+	}
+
+}
+
---
+++ src/CustomerManeger.java
@@ -0,0 +1,25 @@
+
+public class CustomerManeger {
+
+ public void Add() {
+	 System.out.println("müşteri eklendi");
+ }
+		
+	 public void Remove() {
+		 System.out.println("müşteri silindi");
+	 }
+		 		
+	 
+		 
+		 public void Update () {
+			 System.out.println("müşteri güncellendi");	 
+		 }
+		 
+		 
+
+	
+		}
+		 
+	
+
+
