4d1ea48a36a619b74582b7632a35ad0f27895939
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-25 12:24:09 +0300

    Değer ve Referans Tipleri Anlamak and class

--- src/Class1.java
+++ src/Class1.java
@@ -10,6 +10,21 @@ public class Class1 {
 	customerManeger.Remove();
 	customerManeger.Update();
 
+	int sayı1 = 10;
+	int sayı2 = 20;
+	sayı2 = sayı1;
+	sayı1=30;
+	System.out.println(sayı2 );
+	 
+	int[ ] sayılar1 = new int [] {1,2,3};
+	int [] sayılar2= new int [] { 4,5,6};
+	sayılar2=sayılar1;
+	sayılar1[0]=10;
+	System.out.println(sayılar2[0]);
+			
+			
+	
+	
 	}
 
 }
